6be9ab1583aeb3a042ab3524d3c5c09f19470731
GitHub Action <action@github.com>
2023-12-07 14:51:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/jairoguo/gostructtojsonplugin/MyBundle.kt
+++ src/main/kotlin/com/github/jairoguo/gostructtojsonplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jairoguo.gostructtojsonplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/jairoguo/gostructtojsonplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/jairoguo/gostructtojsonplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.jairoguo.gostructtojsonplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
